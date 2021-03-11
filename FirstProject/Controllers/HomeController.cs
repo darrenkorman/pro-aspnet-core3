4d37c0167bd3c06ba39497c9e681e3e0eec63f4e
darrenkorman <darrenkorman@hotmail.com>
2021-03-10 22:28:49 -0800

    First project (#1)
    * Empty solution
    * FirstProject

---
+++ FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            var viewModel = new MyViewModel();
+            return View("MyView", viewModel);
+        }
+    }
+}
---
+++ FirstProject/Models/MyViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace FirstProject.Models
+{
+    public class MyViewModel
+    {
+        private readonly DateTime Now;
+        public string Message { get { return GetMessage(); } }
+        public string CreationTime { get { return Now.ToString(); } }
+
+        public MyViewModel()
+        {
+            this.Now = DateTime.Now;
+        }
+
+        private string GetMessage()
+        {
+            if (Now.Hour < 12)
+            {
+                return "Good Morning";
+            }
+
+            if (Now.Hour < 18)
+            {
+                return "Good Afternon";
+            }
+
+            return "Good Evening";
+        }
+    }
+}
